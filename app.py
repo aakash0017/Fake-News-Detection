754dc952d27583a0260884c3b711a1ca89fb3276
nid989 <nidbhavsar989@gmail.com>
2021-05-13 13:45:44 +0530

    Enhanced Sentence similarity module also added keyword extractractor further devlopement !

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from flask.helpers import make_response
 from flask.wrappers import Response
 import boto3
 
+from model import Sentence_Similarity
+
 client = boto3.client('textract')
 
 app = Flask(__name__)
@@ -20,15 +22,26 @@ def process():
     
     recieved_bytes = bytes(req['image_base64'], 'utf-8')
     Context_sentence = req['Sentence_a']
-    Objective_sentence = req['Sentence_b']
+    # Objective_sentence = req['Sentence_b']
     
-    print(Context_sentence, Objective_sentence)
+    print(Context_sentence)
+    
+    # # create object 
+    sim_checker = Sentence_Similarity(Context_sentence)
+    ref = [
+        "Former President Barack Obama to make his first appearance on The Late Late Show with James Corden",
+        "Michelle Obama’s advice for coping with depression: ‘Develop your own tools’ and give yourself a break",
+        "Barack and Michelle Obama Pay Tribute to Their Family Dog, Bo, Who Died This Weekend",
+        "President Barack Obama To Make Debut Appearance On ‘The Late Late Show With James Corden'"
+    ]
+    sim_checker.getRefrences(list_references=ref)
+    most_sim = sim_checker.getMostSimilar()
+    print(most_sim)
     
     text = image_2_text(recieved_bytes)
     print(text)
     res = make_response(jsonify({"message": "JSON received"}), 200)
     return res
-    
     
 def image_2_text(receivedBytes_base64):
     binary_image = bytearray(base64.decodebytes(receivedBytes_base64))
--- model.py
+++ model.py
@@ -1,17 +1,48 @@
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+# from Keyword_extractor import getKeywords
+
 
 model_name = 'bert-base-nli-mean-tokens'
 model = SentenceTransformer(model_name)
 
-Sentences = [
-    "Obama addressed the press at white house conference",
-    "the press are eager to listen obama's say at the meeting",
-    "Obama talked with the press at the presidential white house meet"
-]
-
-sentence_embeddings = model.encode(Sentences)
-print(cosine_similarity(
-    [sentence_embeddings[0]],
-    sentence_embeddings[1:]
-))
+class Sentence_Similarity:
+    
+    def __init__(self, Context):
+        
+        # context sentence
+        self.Context = Context
+        self.Context_Embedding = model.encode(self.Context)
+        # max_comparisons
+        self.max_comparison = 20
+        
+    def getRefrences(self, list_references):
+        # TODO change list_referneces -> list_keyword and allow web scrapper to return top results.
+        self.References = list_references
+        self.References_Embeddings = model.encode(list_references)
+    
+    def check_Similarity(self):
+        result = cosine_similarity(
+            [self.Context_Embedding],
+            self.References_Embeddings
+        )
+        return result 
+    
+    def getMostSimilar(self):
+        similarity_array = self.check_Similarity()
+        most_similar = self.References[np.argmax(similarity_array)]
+        return most_similar
+        
+            
+if __name__ == "__main__":
+    a = Sentence_Similarity('James corden show eagers the first apperance of Former president Obama')
+    ref = [
+        "Former President Barack Obama to make his first appearance on The Late Late Show with James Corden",
+        "Michelle Obama’s advice for coping with depression: ‘Develop your own tools’ and give yourself a break",
+        "Barack and Michelle Obama Pay Tribute to Their Family Dog, Bo, Who Died This Weekend",
+        "President Barack Obama To Make Debut Appearance On ‘The Late Late Show With James Corden'"
+    ]
+    a.getRefrences(ref)
+    res = a.getMostSimilar()
+    print(res)
